f369efaa86479b0c4ce7f743c34b3a40bb0baf80
anupamxy <123785384+anupamxy@users.noreply.github.com>
2023-07-02 00:14:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,10 +33,46 @@ app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
 
 app.get('/',(req,res)=>{
+  import express from "express";
+import colors from "colours";
+import dotenv from 'dotenv'
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoutes.js';
+import categoryRoutes from './routes/categoryRoute.js';
+import productRoutes from './routes/productRoutes.js';
+import fileUpload from 'express-fileupload';
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+//configure env
+
+dotenv.config();
+
+connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app=express();
+app.use(fileUpload());
+app.use(cors());
+app.use(express.json())
+
+
+app.use(morgan('dev'))
+//routes
+
+app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/category',categoryRoutes);
+app.use('/api/v1/product',productRoutes);
+
+app.get('/',(req,res)=>{
     res.send(
-      
+        res.send("<h1>Welcome to ecommerce app</h1>");
     )
 })
+
 const PORT=process.env.PORT||8080;
 app.listen(PORT,()=>{
     console.log(`SERVER RUNNING on ${process.env.DEV_MODE} mode on port ${PORT}`);
