6cab9278ace91c4d4bbd454e07c7fc5d004f93da
anupamxy <123785384+anupamxy@users.noreply.github.com>
2023-07-02 00:43:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use(morgan('dev'))
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
+
 app.use('*',function (req,res){
   res.sendFile(path.join(__dirname, '../../clientecomm/clientecomm/build/index.html'));
 })
 
- 
 
 
 
