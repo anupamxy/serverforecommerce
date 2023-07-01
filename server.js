652025b3da93daec878f865a016e2f4664736c88
anupamxy <123785384+anupamxy@users.noreply.github.com>
2023-07-01 19:20:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoute.js';
 import productRoutes from './routes/productRoutes.js';
 import fileUpload from 'express-fileupload';
 import cors from "cors";
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 //configure env
